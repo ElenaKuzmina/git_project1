fb1a4800008365d8f062e635d5812bcb0f3dd909
Елена <kuzmina@yandexlyceum.ru>
2020-12-10 16:07:10 +0300

    Добавлены строки 6-8

--- 2.py
+++ 2.py
@@ -3,6 +3,9 @@ def main():
     print('Hello, world!!!')
     print('shgfdkgdkhgdkhs')
     print(55)
+    print('My first git program')
+    print('Hello, world!!!')
+    print(111)
 
 if __name__ == '__main__':
     main()
