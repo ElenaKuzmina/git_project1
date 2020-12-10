3705e50f94d61f00e9e6b7ac51a06bdf3ef9b4f2
Елена <kuzmina@yandexlyceum.ru>
2020-12-10 17:06:41 +0300

    Yandexliceum

--- 1.py
+++ 1.py
@@ -1,9 +1,13 @@
+def my_superfunction():
+    print('YandexLiceum')
+
+
 def main():
     print('My first git program')
     print('And I change it every day')
     print('Again')
     print('UFO came and added this line')
-
+    my_superfunction()
 
 if __name__ == '__main__':
     main()
