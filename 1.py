8f9f8c578e0fab1e9fbec4d60d5a02d3e97a2e18
Елена <kuzmina@yandexlyceum.ru>
2020-12-10 11:20:40 +0300

    Функция my_superfunction() версия2

--- 1.py
+++ 1.py
@@ -3,6 +3,15 @@ def main():
     print('And I change it every day')
     print('Again')
     print('UFO came and added this line')
+    print('My first git program')
+    print('And I change it every day')
+    print('Again')
+    print('UFO came and added this line')
+    my_superfunction()
+
+
+def my_superfunction():
+    print('YandexLyceum')
 
 
 if __name__ == '__main__':
