cf61bfdd448d74cd6491ccff75941ee1555da2a8
Елена <kuzmina@yandexlyceum.ru>
2020-12-10 11:16:12 +0300

    Функция my_superfunction() версия1

--- 1.py
+++ 1.py
@@ -7,6 +7,11 @@ def main():
     print('And I change it every day')
     print('Again')
     print('UFO came and added this line')
+    my_superfunction()
+
+
+def my_superfunction():
+    print('Hello, world!')
 
 
 if __name__ == '__main__':
